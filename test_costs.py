bd8d1109ab250768caadc1f9fbbf6f9bcdff7c39
Mezni <mamezni@gmail.com>
2024-04-12 15:05:53 +0000

    work 2024-04-12

--- test_costs.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from costs import Resource
-
-def test_resource_model_init():
-    id = str(uuid.uuid4())
-    resource = Resource(id=id, name="")
-    assert resource.id == id
-    assert resource.name == ""
--- tests/test_costs.py
+++ tests/test_costs.py
@@ -1,8 +1,8 @@
 import uuid
-from costs.domain.resource import Resource
+from costs.domain import resource as r
 
 def test_resource_model_init():
     code = str(uuid.uuid4())
-    resource = Resource(code=code, name="")
+    resource = r.Resource(code=code, name="")
     assert resource.code == code
     assert resource.name == ""
