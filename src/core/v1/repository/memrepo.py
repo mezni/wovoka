9688aaa460563c6245e476363eb00498864d5bd7
Mezni <mamezni@gmail.com>
2024-04-14 19:23:59 +0000

    work 2024-04-14

--- src/core/v1/repository/memrepo.py
+++
@@ -1,9 +0,0 @@
-from src.core.v1.domain import usage as u
-
-
-class MemRepo:
-    def __init__(self, data):
-        self.data = data
-
-    def list(self):
-        return [u.Usage.from_dict(i) for i in self.data]
