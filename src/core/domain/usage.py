f866a6cac61ded140e85e924736b7c1a35436cf5
Mezni <mamezni@gmail.com>
2024-04-14 14:44:14 +0000

    work 2024-04-14

--- src/core/domain/usage.py
+++ src/core/domain/usage.py
@@ -4,17 +4,38 @@
 class Usage:
     """Definition of the Usage entity"""
 
-    def __init__(self, usage_id, org_id):
+    def __init__(
+        self, usage_id, org_id, source_id, provider, resource_id, resource_name, period
+    ):
         self.usage_id = usage_id
         self.org_id = org_id
+        self.source_id = source_id
+        self.provider = provider
+        self.resource_id = resource_id
+        self.resource_name = resource_name
+        self.period = period
 
     @classmethod
     def from_dict(cls, data):
         """Convert data from a dictionary"""
-        return cls(usage_id=data["usage_id"],org_id=data["org_id"])
+        return cls(
+            usage_id=data["usage_id"],
+            org_id=data["org_id"],
+            source_id=data["source_id"],
+            provider=data["provider"],
+            resource_id=data["resource_id"],
+            resource_name=data["resource_name"],
+            period=data["period"],
+        )
 
     def to_dict(self):
         """Convert data into dictionary"""
         return {
-            "usage_id": self.usage_id,"org_id": self.org_id,
+            "usage_id": self.usage_id,
+            "org_id": self.org_id,
+            "source_id": self.source_id,
+            "provider": self.provider,
+            "resource_id": self.resource_id,
+            "resource_name": self.resource_name,
+            "period": self.period,
         }
--- tests/core/test_usage.py
+++ tests/core/test_usage.py
@@ -4,21 +4,55 @@ from src.core.domain import usage as u
 
 def test_usage_model_init():
     code = v.code
-    usage = u.Usage(usage_id=code,org_id='0661be46-fbd3-79c8-8000-2bf97fb86332')
+    usage = u.Usage(
+        usage_id=code,
+        org_id="0661be46-fbd3-79c8-8000-2bf97fb86332",
+        source_id="0661be77-876d-77e8-8000-afffb7a4ab8d",
+        provider="aws",
+        resource_id="0661be9f-0260-7f5d-8000-6a4da3317708",
+        resource_name="i-082b1a163698b8ede",
+        period="2024-04-14",
+    )
     assert usage.usage_id == code
+    assert usage.org_id == "0661be46-fbd3-79c8-8000-2bf97fb86332"
+    assert usage.source_id == "0661be77-876d-77e8-8000-afffb7a4ab8d"
+    assert usage.provider == "aws"
+    assert usage.resource_id == "0661be9f-0260-7f5d-8000-6a4da3317708"
+    assert usage.resource_name == "i-082b1a163698b8ede"
+    assert usage.period == "2024-04-14"
 
 
 def test_usage_model_from_dict():
     code = v.code
-    usage = u.Usage.from_dict({
-            'usage_id': code,'org_id': '0661be46-fbd3-79c8-8000-2bf97fb86332'})
+    usage = u.Usage.from_dict(
+        {
+            "usage_id": code,
+            "org_id": "0661be46-fbd3-79c8-8000-2bf97fb86332",
+            "source_id": "0661be77-876d-77e8-8000-afffb7a4ab8d",
+            "provider": "aws",
+            "resource_id": "0661be9f-0260-7f5d-8000-6a4da3317708",
+            "resource_name": "i-082b1a163698b8ede",
+            "period": "2024-04-14",
+        }
+    )
     assert usage.usage_id == code
-    
+    assert usage.org_id == "0661be46-fbd3-79c8-8000-2bf97fb86332"
+    assert usage.source_id == "0661be77-876d-77e8-8000-afffb7a4ab8d"
+    assert usage.provider == "aws"
+    assert usage.resource_id == "0661be9f-0260-7f5d-8000-6a4da3317708"
+    assert usage.resource_name == "i-082b1a163698b8ede"
+    assert usage.period == "2024-04-14"
+
+
 def test_usage_model_to_dict():
-    usage_dict =  {
-       'usage_id': v.code,
-       'org_id': '0661be46-fbd3-79c8-8000-2bf97fb86332',
-    } 
+    usage_dict = {
+        "usage_id": v.code,
+        "org_id": "0661be46-fbd3-79c8-8000-2bf97fb86332",
+        "source_id": "0661be77-876d-77e8-8000-afffb7a4ab8d",
+        "provider": "aws",
+        "resource_id": "0661be9f-0260-7f5d-8000-6a4da3317708",
+        "resource_name": "i-082b1a163698b8ede",
+        "period": "2024-04-14",
+    }
     usage = u.Usage.from_dict(usage_dict)
     assert usage.to_dict() == usage_dict
-    
