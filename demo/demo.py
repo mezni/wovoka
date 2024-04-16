e6486d59723f70804fadfcc01cf3983f0d4c5bb0
Mezni <mamezni@gmail.com>
2024-04-16 12:20:10 +0000

    work 2024-04-16

---
+++ demo/demo.py
@@ -0,0 +1,34 @@
+from tinydb import TinyDB       
+db = TinyDB("demodb.json")
+new_item = {"name": "Book", "quantity": 5}
+db.insert(new_item) 
+
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DBConnectionHandler:
+
+    def __init__(self) -> None:
+        self.__connection_string = "sqlite:///test.db"
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+        
+db_conn=DBConnectionHandler()
+db_conn.session.add(new_item)
