b8c9a6f53a055e366e01ebfc7ed8b937a4791383
Mezni <mamezni@gmail.com>
2024-04-16 14:13:57 +0000

    work 2024-04-16

--- src/infra/db_config.py
+++ src/infra/db_config.py
@@ -1,13 +1,27 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 class DBConnectionHandler:
     """sqlalchemy database connection"""
 
     def __init__(self):
-        self.__connection_string = "sqlite:///demo.db"
+        self.__connection_string = "sqlite:///_demo.db"
+        self.__engine = self.__create_database_engine()
         self.session = None
 
-    def get_engine(self):
+    def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
         return engine
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        print(self.session)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
--- tests/infra/test_db.py
+++ tests/infra/test_db.py
@@ -1,5 +1,20 @@
-import src.infra.db_config
+from src.infra.db_config import DBConnectionHandler
+from src.infra.db_base import Base
+from src.entities.users import Users
 
 
-def test_dummy():
-    assert 1 == 1
+def test_db_init():
+    with DBConnectionHandler() as db_conn:
+        engine = db_conn.get_engine()
+        Base.metadata.create_all(engine)
+        new_user = Users(first_name="Mohamed Ali", last_name="MEZNI", age=50)
+        db_conn.session.add(new_user)
+        db_conn.session.commit()
+
+
+def test_db_user_create():
+    with DBConnectionHandler() as db_conn:
+        engine = db_conn.get_engine()
+        new_user = Users(first_name="Mohamed Ali", last_name="MEZNI", age=50)
+        db_conn.session.add(new_user)
+        db_conn.session.commit()
