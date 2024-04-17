6d39af93342bced08c58b34f19fcb8dd1fda95f9
Mezni <mamezni@gmail.com>
2024-04-17 17:44:55 +0000

    work 2024-04-17

--- src/repositories/resource_repo.py
+++
@@ -1,8 +0,0 @@
-from src.entities.resource import Resource
-from src.repositories.resource_repo_abs import AbstractResourceRepository
-
-
-class ResourceRepository(AbstractResourceRepository):
-    def insert(self, data: dict) -> Resource:
-        resource = Resource(**data)
-        return resource
--- src/repositories/resource_repo_abs.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-from src.entities.resource import Resource
-
-
-class AbstractResourceRepository(metaclass=ABCMeta):
-    @abstractmethod
-    def insert(self, data: dict) -> Resource:
-        raise NotImplementedError
