7450f077bc0a7ce92c87cea251a7d00089b97f67
Mezni <mamezni@gmail.com>
2024-04-18 12:28:27 +0000

    work 2024-04-18

--- tests/test_user_entity.py
+++
@@ -1,8 +0,0 @@
-from src.user import User
-
-
-def test_user_entity():
-    user = User(user_id=1, name="dali", email="dali@example.com")
-    assert user.user_id == 1
-    assert user.name == "dali"
-    assert user.email == "dali@example.com"
