1f7d7fe2e0586e5c1bf1f587f017fa38ffe85a7b
Mezni <mamezni@gmail.com>
2024-04-19 20:24:18 +0000

    work 2024-04-19

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ class PeriodInputDto:
 
 @dataclass
 class PeriodOutputDto:
-    """Output Dto for create profession"""
+    """Output Dto for create period"""
 
     period: Period
 
@@ -136,34 +136,147 @@ class PeriodRepository:
         return {"period_min": period_min, "period_max": period_max}
 
     def list(self):
-        return self.periods
+        periods_sorted = sorted(self.periods, key=lambda x: x.period_date)
+        return periods_sorted
+
+
+@dataclass
+class Resource:
+    """Definition of the Resource entity"""
+
+    resource_code: uuid.UUID
+    resource_id: str
+    resource_name: str
+
+    @classmethod
+    def from_dict(cls, data):
+        """Convert data from a dictionary"""
+        return cls(**data)
+
+    def to_dict(self):
+        """Convert data into dictionary"""
+        return asdict(self)
+
+
+@dataclass
+class ResourceInputDto:
+    resource_id: str
+    resource_name: str
+
+    def to_dict(self):
+        """Convert data into dictionary"""
+        return asdict(self)
+
+
+@dataclass
+class ResourceOutputDto:
+    """Output Dto for create resource"""
+
+    resource: Resource
+
+
+class ResourcePresenter:
+    @staticmethod
+    def dto_to_entity(period_dto):
+        resource_code = uuid.uuid4()
+        resource_id = period_dto.resource_id
+        resource_name = period_dto.resource_name
+
+        resourc = Resource(resource_code, resource_id, resource_name)
+        return resourc
+
+
+class ResourceRepository:
+    def __init__(self):
+        self.resources = []
+
+    def add(self, resource):
+        self.resources.append(resource)
+
+    def get_resource_by_id_and_name(self,resource_id=None, resource_name=None):
+        for r in self.resources:
+            if resource_id and resource_name:
+                if r.resource_id == resource_id and r.resource_name == resource_name:
+                    return r
+            elif resource_id:
+                if r.resource_id == resource_id:
+                    return r                
+            elif resource_name:
+                if r.resource_name == resource_name:
+                    return r                   
+        
+
+    def list(self):
+        return self.resources
 
 
 def main():
     data = [
-        {"period_name": "2024-04-03"},
-        {"period_name": "2024-04-06"},
-        {"period_name": "2024-04-06"},
-        {"period_name": "2024-04-01"},
+        {
+            "period_name": "2024-04-02",
+            "resource_id": "i-5as6asd4f5sadf",
+            "resource_name": "ec2-dali-devbox",
+        },
+        {
+            "period_name": "2024-04-02",
+            "resource_id": "i-5as6asd4f5sadf",
+            "resource_name": "ec2-dali-devbox",
+        },
+        {
+            "period_name": "2024-04-03",
+            "resource_id": "i-5as6asd4f5sadf",
+            "resource_name": "ec2-dali-devbox",
+        },
+        {
+            "period_name": "2024-04-06",
+            "resource_id": "i-5as6asd4f5sadf",
+            "resource_name": "ec2-dali-devbox",
+        },
+        {
+            "period_name": "2024-04-06",
+            "resource_id": "i-5as6asd4f5sadf",
+            "resource_name": "ec2-dali-devbox",
+        },
+        {
+            "period_name": "2024-04-01",
+            "resource_id": "i-5as6asd4f5sadf",
+            "resource_name": "ec2-dali-devbox",
+        },
     ]
     period_repo = PeriodRepository()
 
     for d in data:
-        print(d)
-        period_saved = period_repo.get_period_by_name(d["period_name"])
+        period_name = d["period_name"]
+        period_saved = period_repo.get_period_by_name(period_name)
         if period_saved:
             period = period_saved
         else:
-            period_gap = period_repo.get_interval_gap(d["period_name"])
+            period_gap = period_repo.get_interval_gap(period_name)
             for p in period_gap:
                 period_dto = PeriodInputDto(p)
                 period = PeriodPresenter.dto_to_entity(period_dto)
-                print(f"    = {period.period_name}")
                 period_repo.add(period)
+            period = period_repo.get_period_by_name(period_name)
 
     period_list = period_repo.list()
     for p in period_list:
         print(f"period= {p.period_code} - {p.period_name} - {p.period_date}")
 
+    resource_repo = ResourceRepository()
+    for d in data:
+        resource_id = d["resource_id"]
+        resource_name = d["resource_name"]
+        resource_saved = resource_repo.get_resource_by_id_and_name(resource_id, resource_name)
+        print (resource_saved)
+# period_saved = period_repo.get_period_by_name(period_name)
 
+        print(resource_id, resource_name)
+        resource_dto = ResourceInputDto(resource_id, resource_name)
+        resource = ResourcePresenter.dto_to_entity(resource_dto)
+        resource_repo.add(resource)
+
+
+    resource_list = resource_repo.list()
+    for r in resource_list:
+        print(f"period= {r.resource_code} - {r.resource_id} - {r.resource_name}")
 main()
