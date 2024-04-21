00a0f9c7b84b5001e4c37fd668c16ee67f3dd761
Mezni <mamezni@gmail.com>
2024-04-21 16:30:19 +0000

    work 2024-04-21

--- src/interactor/use_cases/periods_interval_load_usecase.py
+++ src/interactor/use_cases/periods_interval_load_usecase.py
@@ -1,15 +1,21 @@
 """ This module is responsible for load Period interval.
 """
 
+import uuid
 from typing import List
 from src.domain.entities.period import Period
+from src.interactor.interfaces.periods_repository_interface import (
+    PeriodRepositoryInterface,
+)
 
 
 class LoadPeriodIntervalUseCase:
     """This class is responsible for load Period interval."""
 
-    def __init__(self):
-        pass
+    def __init__(self, period_repo: PeriodRepositoryInterface):
+        self.period_repo = period_repo
 
     def execute(self, period_list: List[str]) -> List[Period]:
+        period = Period(uuid.uuid4(), period_list[0])
+        self.period_repo.create_period(period)
         return []
