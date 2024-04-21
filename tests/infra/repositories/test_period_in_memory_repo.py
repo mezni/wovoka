959d6a79be99869d783ec8a6d69acd4857c1136d
Mezni <mamezni@gmail.com>
2024-04-21 12:51:57 +0000

    work 2024-04-21

--- tests/infra/repositories/test_period_in_memory_repo.py
+++
@@ -1,8 +0,0 @@
-from src.domain.entities.period import Period
-from src.infra.repositories.period_in_memory_repo import InMemoryPeriodRepository
-
-
-def test_period_in_memory_repo():
-    repo = InMemoryPeriodRepository()
-    result = repo.get_periods_interval()
-    assert len(result) == 0
