ace4ea572fe3c9e354c28e9f11877003d2324340
Mezni <mamezni@gmail.com>
2024-04-21 12:23:16 +0000

    work 2024-04-21

--- tests/infra/repositories/test_period_in_memory_repository.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import pytest
-from src.infra.repositories.period_in_memmory_repository import PeriodInMemoryRepository
-
-
-def test_profession_in_memory_repository():
-    repository = PeriodInMemoryRepository()
-
-    assert len(repository._periods) == 0
