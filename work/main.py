fa1b36001fe8edec3e080a4ff85b3bedb73af3c2
Mezni <mamezni@gmail.com>
2024-04-23 16:46:39 +0000

    work 2024-04-23

--- work/main.py
+++ work/main.py
@@ -4,40 +4,11 @@ from datetime import datetime, timedelta
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from src.database import init_db, get_session
-from src.models import Period as PeriodModel
+from src.models import PeriodModel
 from src.entities import Period
 from src.schemas import CreatePeriodIn
 from sqlalchemy import select
-
-
-async def create_moteur(
-    async_session: async_sessionmaker[AsyncSession], period_name: str
-):
-    item_db = PeriodModel(period_code=uuid.uuid4(), period_name=period_name)
-    async with async_session() as session:
-        async with session.begin():
-            session.add(item_db)
-            return item_db
-
-async def list_periods(
-    async_session: async_sessionmaker[AsyncSession]
-):
-    async with async_session() as session:
-        stmt = select(PeriodModel)  # .order_by(A.id).options(selectinload(A.bs))
-        result = await session.execute(stmt)  
-        return result
-# async def period_uc(period_dto):
-#    x=Period(period_code=uuid.uuid4(), period_name=period_dto.period_name)
-#    return x
-
-
-async def periods_interval(
-    async_session: async_sessionmaker[AsyncSession],
-    period_name_min: str,
-    period_name_max: str,
-):
-    pass
-
+from typing import List
 
 async def get_periods_interval(period_list):
     period_name_min = None
@@ -60,39 +31,51 @@ async def get_periods_interval(period_list):
     return period_name_min, period_name_max
 
 
-async def period_uc(period_list):
-    session = await get_session()
-    period_name_min, period_name_max = await get_periods_interval(period_list)
-    x=await create_moteur(session,period_name_min)
+async def load_periods(
+    async_session: async_sessionmaker[AsyncSession],
+    period_name_min: str,
+    period_name_max: str,
+):
     total_days = (
         datetime.strptime(period_name_max, "%Y-%m-%d")
         - datetime.strptime(period_name_min, "%Y-%m-%d")
     ).days + 1
-    result=await list_periods(session)
-    for a in result.scalars():
-        print (a.period_code)
-    
+    async with async_session() as session:
+        for day_number in range(total_days):
+            current_date = (
+                datetime.strptime(period_name_min, "%Y-%m-%d")
+                + timedelta(days=day_number)
+            ).date()
+            print(current_date)
+            session.add(PeriodModel(period_code=uuid.uuid4(),period_name=current_date.strftime("%Y-%m-%d")))
+            await  session.commit()
+            stmt = select(PeriodModel)#.where(PeriodModel.period_name == current_date.strftime("%Y-%m-%d"))
+            result = await session.execute(stmt)
 
 
-#    for day_number in range(total_days):
-#        current_date = (
-#            datetime.strptime(period_name_min, "%Y-%m-%d") + timedelta(days=day_number)
-#        ).date()
-#        print(current_date)
-
+async def list_periods(
+    async_session: async_sessionmaker[AsyncSession],
+    period_name_min: str,
+    period_name_max: str,
+):
+    total_days = (
+        datetime.strptime(period_name_max, "%Y-%m-%d")
+        - datetime.strptime(period_name_min, "%Y-%m-%d")
+    ).days + 1
+    async with async_session() as session:
+        stmt = select(PeriodModel)#.where(PeriodModel.period_name == current_date.strftime("%Y-%m-%d"))
+        result = await session.execute(stmt)
+        return result.fetchall()
 
 async def main():
     await init_db()
     session = await get_session()
-    period_list = ["2024-04-20", "2024-04-10", "2024-04-01"]
-    uc = await period_uc(period_list)
+    period_list = ["2024-04-10", "2024-04-04", "2024-04-01"]
+    period_name_min, period_name_max = await get_periods_interval(period_list)
 
+    await load_periods(session, period_name_min, period_name_max)
 
-#    period_name = "2024-04-05"
-#    period_dto=CreatePeriodIn(period_name)
-#    uc=await period_uc(period_dto)
-#    print (uc.period_day)
-#    await create_moteur(session, "2024-04-05")
-
+    xx=await list_periods(session, period_name_min, period_name_max)
+    print (xx)
 
 asyncio.run(main())
--- work/src/models.py
+++ work/src/models.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
-class Period(Base):
+class PeriodModel(Base):
     __tablename__ = "periods"
     period_code = Column(UUID, primary_key=True, index=True)
     period_name = Column(String(60), unique=True, nullable=False, index=True)
