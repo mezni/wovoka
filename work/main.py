b40e47eb99104fa1ec35ed008de8e6292f2ae8f9
Mezni <mamezni@gmail.com>
2024-04-25 20:40:31 +0000

    work 2024-04-25

--- work/main.py
+++ work/main.py
@@ -1,27 +1,8 @@
 import asyncio
 
 
-class MemCliProcessHandler:
-    """Memomy Client Process Handler"""
-
-    def __init__(self) -> None:
-        pass
-
-    def execute(self, usage_data) -> None:
-        """
-        Execute the ProcessHandler
-
-        Args:
-            usage_data ([str]): list of periods
-        Returns:
-            None
-        """
-        print("# process")
-
-
 async def main():
-
-    usage_data = ["2024-04-12", "2024-04-12", "2024-04-22"]
+    pass
 
 
 asyncio.run(main())
