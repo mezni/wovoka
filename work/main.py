0a146527ca0fc491f602521796c65e4330d781ab
Mezni <mamezni@gmail.com>
2024-04-25 12:18:49 +0000

    work 2024-04-25

--- work/main.py
+++ work/main.py
@@ -1,15 +1,8 @@
 import asyncio
-from src.usecases import CreatePeriodInterval
 
 
 async def main():
     print("# Enter")
-    period_interval_repo = None
-    create_period_interval_usecase = CreatePeriodInterval(period_interval_repo)
-    period_interval_input = ["2024-04-24", "2024-04-10"]
-    period_interval_output = await create_period_interval_usecase.execute(
-        period_interval_input
-    )
 
 
 asyncio.run(main())
