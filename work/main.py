fdd69dd72c319846cd8fe25f16fb55b012f889ac
Mezni <mamezni@gmail.com>
2024-04-25 15:56:36 +0000

    work 2024-04-25

--- work/main.py
+++ work/main.py
@@ -1,13 +1,8 @@
 import asyncio
-from src.usecases import LoadUsageUseCase
 
 
 async def main():
     print("# Enter")
-    usage_data = ["2024-04-25", "2024-04-10", "2024-04-11x"]
-    repos = None
-    load_usage_usecase = LoadUsageUseCase(repos)
-    await load_usage_usecase.process(usage_data)
 
 
 asyncio.run(main())
