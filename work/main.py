5436ebde4dac1be9e5396fc8061dfa9eab68c8d4
Mezni <mamezni@gmail.com>
2024-04-25 19:51:01 +0000

    work 2024-04-25

--- work/main.py
+++ work/main.py
@@ -1,7 +1,5 @@
 import asyncio
 
-from src.app.mem_cli.usage_loader_controller import UsageLoaderController
-
 
 class MemCliProcessHandler:
     """Memomy Client Process Handler"""
@@ -18,16 +16,12 @@ class MemCliProcessHandler:
         Returns:
             None
         """
-        controller = UsageLoaderController()
-        controller.execute(usage_data)
         print("# process")
 
 
 async def main():
 
     usage_data = ["2024-04-12", "2024-04-12", "2024-04-22"]
-    process = MemCliProcessHandler()
-    process.execute(usage_data)
 
 
 asyncio.run(main())
