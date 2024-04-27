9433d54e91167b4e6a6c082c62176dfa0abe1ffb
Mezni <mamezni@gmail.com>
2024-04-27 00:34:27 +0000

    work 2024-04-26

--- work/main.py
+++ work/main.py
@@ -1,15 +1,59 @@
 import asyncio
+import uuid
+from typing import TypeVar
+from datetime import datetime
+from pydantic import BaseModel
+from abc import ABC, abstractmethod
+from tinydb import TinyDB, Query
 
-from src.infra.logger import Logger
-from src.app.in_memory_controller import CreateProviderController
+
+UUIDType = TypeVar("UUIDType", bound=uuid.UUID)
+
+
+def generate_uuid() -> UUIDType:
+    return uuid.uuid4()
+
+
+class Provider(BaseModel):
+    provider_code: str
+    provider_name: str
+
+
+class CatalogRepository(ABC):
+    @abstractmethod
+    def create_provider(self, post):
+        pass
+
+
+class TinyDBCatalogRepository(CatalogRepository):
+    def __init__(self, db_path):
+        self.db = TinyDB(db_path)
+        self.providers_table = self.db.table("providers")
+
+    def create_provider(self, provider):
+        self.providers_table.insert(
+            {
+                "provider_code": provider.provider_code,
+                "provider_name": provider.provider_name,
+            }
+        )
+
+
+class CatalogInteractor:
+    def __init__(self, catalog_repo):
+        self.catalog_repo = catalog_repo
+
+    def create_provider(self, provider_name):
+        code = generate_uuid()
+        provider = Provider(provider_code=str(code), provider_name=provider_name)
+        self.catalog_repo.create_provider(provider)
 
 
 async def main():
-    """In Memory controller"""
-    data = []
-    logger = Logger()
-    controller = CreateProviderController(logger)
-    await controller.execute(data)
+    catalog_repo = TinyDBCatalogRepository("_catalog.json")
+    catalog_interactor = CatalogInteractor(catalog_repo)
+    catalog_interactor.create_provider(provider_name="aws")
+    catalog_interactor.create_provider(provider_name="azure")
 
 
 asyncio.run(main())
--- work/src/infra/logger.py
+++
@@ -1,8 +0,0 @@
-from src.interractor.interfaces import LoggerInterface
-
-
-class Logger(LoggerInterface):
-    """Logger"""
-
-    def __init__(self):
-        pass
