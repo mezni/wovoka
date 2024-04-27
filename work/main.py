3f4d42c1b2cfe2dbb37f7588c71a678c6c01e11d
Mezni <mamezni@gmail.com>
2024-04-27 15:43:03 +0000

    work 2024-04-27

--- work/main.py
+++ work/main.py
@@ -1,12 +1,22 @@
+import uuid
 import asyncio
 
 from src.infra.sqlite.config import create_db_engine, init_db
+from src.infra.sqlite.repositories.sqlite_provider_repository import (
+    SqliteProviderRepository,
+)
 
 
 async def main():
     db_url = "sqlite+aiosqlite:///_usage.db"
     engine = await create_db_engine(db_url)
     await init_db(engine)
+    provider_repo = SqliteProviderRepository(engine)
+    payload = {"provider_code": uuid.uuid4(), "provider_name": "oci"}
+    await provider_repo.create_provider(payload)
+    x = await provider_repo.get_all_provider()
+    for i in x:
+        print(i.provider_code, i.provider_name)
 
 
 asyncio.run(main())
--- work/src/infra/sqlite/repositories/sqlite_provider_repository.py
+++ work/src/infra/sqlite/repositories/sqlite_provider_repository.py
@@ -1,21 +1,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Dict
+from sqlalchemy.future import select
+from src.infra.sqlite.models.provider import ProviderModel
 
 
 class SqliteProviderRepository:
-    def __init__(self, db_url):
-        self.engine = create_async_engine(db_url, echo=True, future=True)
+    def __init__(self, engine):
+        self.engine = engine
         self.AsyncSessionLocal = sessionmaker(
             bind=self.engine, class_=AsyncSession, expire_on_commit=False
         )
 
-    async def create_post(self, post):
+    async def create_provider(self, payload: Dict):
         async with self.AsyncSessionLocal() as session:
             async with session.begin():
-                session.add(post)
+                item = ProviderModel(**payload)
+                session.add(item)
 
-    async def get_all_posts(self):
+    async def get_all_provider(self):
         async with self.AsyncSessionLocal() as session:
             async with session.begin():
-                return await session.execute(Post.__table__.select()).scalars().all()
+                q = await session.execute(select(ProviderModel))
+                result = q.scalars().all()
+                return result
+
+
+#    async def create_provider(self, payload):
+#        async with self.AsyncSessionLocal() as session:
+#            async with session.begin():
+#                session.add(payload)
+
+#    async def get_all_provider(self):
+#        async with self.AsyncSessionLocal() as session:
+#            async with session.begin():
+#                return await session.execute(Post.__table__.select()).scalars().all()
