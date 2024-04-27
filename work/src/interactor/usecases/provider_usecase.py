e4ae66db2e73d6eb142ae1e3b49ca4b74ae70874
Mezni <mamezni@gmail.com>
2024-04-27 21:24:38 +0000

    work 2024-04-27

--- work/src/interactor/usecases/provider_usecase.py
+++
@@ -1,9 +0,0 @@
-from src.interactor.repository.provider_repository import ProviderRepositoryInterface
-
-
-class ProviderCreateUsecase:
-    def __init__(self, repo):
-        self.repo = repo
-
-    async def create(self, provider_name: str):
-        await self.repo.create_provider(provider_name)
