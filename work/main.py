c15f79a23442ed4c4eb98860cb062f4025b0d277
Mezni <mamezni@gmail.com>
2024-04-28 23:41:32 +0000

    work 2024-04-28

--- work/main.py
+++ work/main.py
@@ -1,9 +1,19 @@
 import uuid
 import asyncio
-
+import csv
 
 async def main():
     db_url = "sqlite+aiosqlite:///_usage.db"
+    
+    with open("aws_data.csv", "r") as f:
+        reader = csv.DictReader(f)
+        data = list(reader)
+    
+    clients = list(set([x['Client'] for x in data]))
+    print (clients)
 
-
+#    dates = list(set([x['Date'] for x in data]))
+#    print (dates)
 asyncio.run(main())
+
+
