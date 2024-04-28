132270bdfc0742b484ce3365c140e71ff005f3a7
Mezni <mamezni@gmail.com>
2024-04-28 23:21:48 +0000

    work 2024-04-28

--- work/src/domain/provider.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel
-from src.domain.types import UUIDType
-
-
-class Provider(BaseModel):
-
-    provider_code: UUIDType
-    provider_name: str
