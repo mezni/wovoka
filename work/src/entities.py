8f3c099ddc2ff3c572a8ef50ba87846f9c89c02a
Mezni <mamezni@gmail.com>
2024-04-30 12:26:51 +0000

    work 2024-04-30

---
+++ work/src/entities.py
@@ -0,0 +1,15 @@
+import uuid
+from typing import TypeVar
+from pydantic import BaseModel
+
+
+UUIDType = TypeVar("UUIDType", bound=uuid.UUID)
+
+
+
+
+class Task(BaseModel):
+
+    task_code: UUIDType
+    task_name: str
+    task_description: str
