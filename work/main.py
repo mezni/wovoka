fe2ac45fcab240279c7545e8d31d5a4553f2e6b6
Mezni <mamezni@gmail.com>
2024-05-01 13:29:23 +0000

    work 2024-05-01

--- work/main.py
+++ work/main.py
@@ -1,8 +1,35 @@
 import asyncio
+from abc import ABC, abstractmethod
+
+
+class InputReader(ABC):
+    @abstractmethod
+    def read_source(self, source_path):
+        raise NotImplementedError
+
+
+class CSVReader(InputReader):
+    def read_source(self, source_path):
+        data = []
+        return data
+
+
+class InputReaderUseCase:
+    def __init__(self, input_reader):
+        self.input_reader = input_reader
+
+    def execute(self, source_path):
+        return self.input_reader.read_source(source_path)
 
 
 async def main():
-    pass
+    source_type = "csv"
+    source_path = ""
+    if source_type == "csv":
+        input_reader = CSVReader("source_path")
+
+    input_reader_usecase = InputReaderUseCase(input_reader)
+    input_reader_usecase.execute(source_path)
 
 
 asyncio.run(main())
