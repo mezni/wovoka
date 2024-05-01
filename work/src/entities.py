b3c32cbed58a10733aaf6d584713b851b89ba2b2
Mezni <mamezni@gmail.com>
2024-05-01 12:41:44 +0000

    work 2024-05-01

--- work/src/entities.py
+++ work/src/entities.py
@@ -1,15 +1,35 @@
 import uuid
-from typing import TypeVar
+from typing import TypeVar, List
 from pydantic import BaseModel
 
 
 UUIDType = TypeVar("UUIDType", bound=uuid.UUID)
 
 
-class Task(BaseModel):
+class Tag(BaseModel):
+    tag_code: UUIDType
+    tag_type: str
+    tag_name: str
+    tag_value: str
 
-    task_code: UUIDType
-    task_name: str
-    task_description: str
+
+class Cost(BaseModel):
+    cost_code: UUIDType
+    cost_type: str
+    cost_value: float
+    cost_currency: str
 
 
+class CostRecord(BaseModel):
+
+    cost_code: UUIDType
+    org_name: str
+    provider_name: str
+    period_name: str
+    account_id: str
+    account_name: str
+    service_name: str
+    resource_id: str
+    resource_name: str
+    tags: List[Tag]
+    cost: List[Cost]
