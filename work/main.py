953d5f2414347596c084ecca97c98b20bb353fa4
Mezni <mamezni@gmail.com>
2024-05-02 14:22:27 +0000

    work 2024-05-02

--- work/main.py
+++ work/main.py
@@ -36,37 +36,34 @@ class UseCase:
         self.regions = []
         self.resources = []
 
+    def _get_distinct(self, df, cols):
+        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+        return df_temp.to_dict("records")
+
+    def _load_structs(self, df):
+        self.orgs = self._get_distinct(df, ["org_name"])
+        self.periods = self._get_distinct(df, ["period_name"])
+        self.accounts = self._get_distinct(
+            df, ["org_name", "provider_name", "account_name"]
+        )
+        self.services = self._get_distinct(df, ["provider_name", "service_name"])
+        self.regions = self._get_distinct(df, ["provider_name", "region_name"])
+        self.resources = self._get_distinct(
+            df,
+            [
+                "org_name",
+                "provider_name",
+                "account_name",
+                "resource_name",
+                "resource_id",
+            ],
+        )
+
     def execute(self, data: List[Dict]):
         df = pd.DataFrame(data)
-        cols = ["org_name"]
-        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        self.orgs = df_temp.to_dict("records")
-
-        cols = ["period_name"]
-        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        self.periods = df_temp.to_dict("records")
-
-        cols = ["org_name", "provider_name", "account_name"]
-        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        self.accounts = df_temp.to_dict("records")
-
-        cols = ["provider_name", "service_name"]
-        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        self.services = df_temp.to_dict("records")
-
-        cols = ["provider_name", "region_name"]
-        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        self.regions = df_temp.to_dict("records")
-
-        cols = [
-            "org_name",
-            "provider_name",
-            "account_name",
-            "resource_name",
-            "resource_id",
-        ]
-        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        self.resources = df_temp.to_dict("records")
+        self._load_structs(df)
+        for org in self.orgs:
+            print(org)
 
 
 async def main():
@@ -88,7 +85,6 @@ async def main():
     costs = validator.validate(data)
     usecase = UseCase()
     usecase.execute(costs)
-    print(usecase.resources)
 
 
 asyncio.run(main())
