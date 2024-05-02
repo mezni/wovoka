6d7c324149a5003c301324776f8ccbb9e4409951
Mezni <mamezni@gmail.com>
2024-05-02 14:09:17 +0000

    work 2024-05-02

--- work/main.py
+++ work/main.py
@@ -28,11 +28,45 @@ class Validator:
 
 
 class UseCase:
+    def __init__(self):
+        self.orgs = []
+        self.periods = []
+        self.accounts = []
+        self.services = []
+        self.regions = []
+        self.resources = []
+
     def execute(self, data: List[Dict]):
         df = pd.DataFrame(data)
-        cols = ["org_name", "service_name"]
+        cols = ["org_name"]
         df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
-        print(df_temp.to_dict("records"))
+        self.orgs = df_temp.to_dict("records")
+
+        cols = ["period_name"]
+        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+        self.periods = df_temp.to_dict("records")
+
+        cols = ["org_name", "provider_name", "account_name"]
+        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+        self.accounts = df_temp.to_dict("records")
+
+        cols = ["provider_name", "service_name"]
+        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+        self.services = df_temp.to_dict("records")
+
+        cols = ["provider_name", "region_name"]
+        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+        self.regions = df_temp.to_dict("records")
+
+        cols = [
+            "org_name",
+            "provider_name",
+            "account_name",
+            "resource_name",
+            "resource_id",
+        ]
+        df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+        self.resources = df_temp.to_dict("records")
 
 
 async def main():
@@ -54,6 +88,7 @@ async def main():
     costs = validator.validate(data)
     usecase = UseCase()
     usecase.execute(costs)
+    print(usecase.resources)
 
 
 asyncio.run(main())
