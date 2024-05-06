835ba0652002866224e578b5fae1f24976bde5a8
Mezni <mamezni@gmail.com>
2024-05-06 20:32:33 +0000

    work 2024-05-06

--- work/main.py
+++ work/main.py
@@ -2,8 +2,8 @@ import uuid
 import asyncio
 from src.infra.SQLiteDatabase import SQLiteDatabase
 from src.infra.adapters.provider_repository import SQLiteProviderRepository
-from src.interactor.usecases.provider_usecase import AddProviderUseCase
-from src.app.controllers.provider_controller import ProviderController
+from src.interactor.usecases.provider_usecase import ProviderUseCase
+#from src.app.controllers.provider_controller import ProviderController
 
 
 async def main():
@@ -12,11 +12,14 @@ async def main():
     await sql_db.init_db()
     async_session = await sql_db.get_session()
     provider_repo = SQLiteProviderRepository(async_session)
-    payload = {"provider_code": uuid.uuid4(), "provider_name": "AWS"}
-    ret = await provider_repo.add_provider(payload)
-    print(ret)
+#    payload = {"provider_code": uuid.uuid4(), "provider_name": "Azure"}
+#    ret = await provider_repo.add_provider(payload)
+#    print(ret)
 
-
+    provider_usecase = ProviderUseCase(provider_repo)
+    xxx=await provider_usecase.execute("Oci")
+    print (xxx)
+    
 #    provider_repo = SQLiteProviderRepository(async_session)
 #    add_provider_usecase = AddProviderUseCase(provider_repo)
 #    provider_controller = ProviderController(add_provider_usecase)
--- work/src/interactor/usecases/provider_usecase.py
+++ work/src/interactor/usecases/provider_usecase.py
@@ -1,10 +1,13 @@
+import uuid
 from src.domain.provider import Provider
 from src.interactor.interfaces.provider_repository import ProviderRepository
 
 
-class AddProviderUseCase:
+class ProviderUseCase:
     def __init__(self, repo: ProviderRepository):
         self.repo = repo
 
+
     async def execute(self, provider_name: str) -> Provider:
-        return await self.repo.add_provider(provider_name)
+        payload = {"provider_code": uuid.uuid4(), "provider_name": provider_name}
+        return await self.repo.add_provider(payload)
