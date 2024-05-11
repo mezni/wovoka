0049f021cc4536a70c06e7d9dc8befc29cf53e76
Mezni <mamezni@gmail.com>
2024-05-11 13:44:01 +0000

    work 2024-05-11

--- work/dwh/load_controller.py
+++ work/dwh/load_controller.py
@@ -1,13 +1,17 @@
 import asyncio
+import pandas as pd
 from db_config import init_db, create_db_engine
 
+
+def get_distinct( df, cols):
+    df_temp = df.groupby(cols).size().reset_index(name="Freq")[cols]
+    return df_temp.to_dict("records")
 
 async def execute():
     db_url = "sqlite+aiosqlite:///_store/_dwh.db"
     engine = await create_db_engine(db_url)
-    await init_db(engine)
-
-
-
+    df = pd.read_csv("_data/data.csv")
+    orgs =  get_distinct( df, ["Client"])
+    print (orgs)
 
 asyncio.run(execute())
