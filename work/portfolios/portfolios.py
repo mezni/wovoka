2d2ab0923c34d6603738de6683e26c755d117ab4
Mezni <mamezni@gmail.com>
2024-05-20 14:18:11 +0000

    work 2024-05-20

--- work/portfolios/portfolios.py
+++ work/portfolios/portfolios.py
@@ -1,7 +1,6 @@
-from __future__ import annotations
 import uuid
-from typing import TypeVar, Optional, Dict, List
 from pydantic import BaseModel
+from typing import TypeVar, Optional
 from datetime import datetime, date
 
 UUIDType = TypeVar("UUIDType", bound=uuid.UUID)
@@ -9,49 +8,24 @@ UUIDType = TypeVar("UUIDType", bound=uuid.UUID)
 
 def generate_uuid() -> UUIDType:
     return uuid.uuid4()
-
-
-class Portfolio(BaseModel):
-    portfolio_code: UUIDType
+    
+class BaseDomainModel(BaseModel):
+    code: UUIDType = generate_uuid()
+#    class Config:
+#        orm_mode = True
+        
+class Portfolio(BaseDomainModel):
     portfolio_name: str
     portfolio_type: str
-    portfolio_parent: Optional[UUIDType]
-
-
-class PortfolioInputDto(BaseModel):
-    portfolio_code: UUIDType = None
-    portfolio_name: str
-    portfolio_type: str
-    portfolio_parent: UUIDType = None
-
-    def model_post_init(self, __context) -> None:
-        self.portfolio_code = generate_uuid()
-
-
-class PortfolioUsecase:
-    def __init__(self) -> None:
-        pass
-
-    def addPortfolio(self, payload: Dict) -> Portfolio:
-        item = Portfolio(**payload)
-        return item
-
-    def addAllocation(self, payload: Dict) -> CostAllocation:
-        item = CostAllocation(**payload)
-        return item
-
-
-class CostAllocation(BaseModel):
-    allocation_code: UUIDType
-    allocation_amount: float
+    portfolio_parent: Optional[UUIDType]=None
+    
+class CostAllocation(BaseDomainModel):
+    allocation_amount: float = 0.0
     allocation_date: date
     portfolio_code: UUIDType
-
-
-if __name__ == "__main__":
-    portfolio_usecase = PortfolioUsecase()
-    portfolio_input_dto = PortfolioInputDto(
-        portfolio_name="default", portfolio_type="root"
-    )
-    portfolio = portfolio_usecase.addPortfolio(portfolio_input_dto.model_dump())
-    print(portfolio.model_dump())
+    
+portfolio_default = Portfolio(portfolio_name="default",portfolio_type="root")
+portfolio_it = Portfolio(portfolio_name="IT",portfolio_type="department",portfolio_parent=portfolio_default.model_dump()["code"])
+portfolio_sales = Portfolio(portfolio_name="Sales",portfolio_type="department",portfolio_parent=portfolio_default.model_dump()["code"])
+portfolio_phenix = Portfolio(portfolio_name="Phenix",portfolio_type="project",portfolio_parent=portfolio_it.model_dump()["code"])
+portfolio_inventory = Portfolio(portfolio_name="Inventory",portfolio_type="project",portfolio_parent=portfolio_it.model_dump()["code"])
