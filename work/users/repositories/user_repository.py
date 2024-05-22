a43bef3a3646f8131e520b25b92d8fa3d80a830d
Mezni <mamezni@gmail.com>
2024-05-22 13:19:20 +0000

    work 2024-05-22

--- work/users/repositories/user_repository.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from abc import ABC, abstractmethod
-
-
-class UserRepositoryInterface(ABC):
-    @abstractmethod
-    async def create(self, payload: Dict):
-        pass
--- work/users/usecases/create_user.py
+++
@@ -1,6 +0,0 @@
-class CreateUser:
-    def __init__(self, repo):
-        self.repo = repo
-
-    def execute(self, user):
-        self.repo.create(user)
