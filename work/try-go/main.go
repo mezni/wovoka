4e42f9f50cb0fa68ab961d2aa56f2f4a8939f789
Mezni <mamezni@gmail.com>
2024-05-24 21:52:51 +0000

    work 2024-05-24

--- work/try-go/main.go
+++ work/try-go/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/mezni/try-go/aggregates"
+)
+
+type Test struct {
+	ID      uuid.UUID
+	Name    string
+	OwnerID *uuid.UUID
+}
 
 func main() {
 	fmt.Println("Hello World")
+	p := aggregates.NewPortfolio("Test", uuid.New(), uuid.New())
+	fmt.Println(p)
+	x := Test{
+		ID:      uuid.New(),
+		Name:    "Test",
+		OwnerID: nil,
+	}
+	fmt.Println(x)
 }
