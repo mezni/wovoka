39289f411745538141061387585f3fdddf25310b
Mezni <mamezni@gmail.com>
2024-05-27 12:35:33 +0000

    work 2024-05-27

--- work/users-go/main.go
+++ work/users-go/main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/mezni/users-go/memrepo"
-	"github.com/mezni/users-go/services"
-	//	"time"
 )
 
 func main() {
 	fmt.Println("- start")
-	repo := memory_repo.NewInMemoryPortfolioRepository()
-	service := services.NewPortfolioService(repo)
-	fmt.Println(repo)
-	service.AddPortfolio("Test")
-	service.AddPortfolio("Test1")
-	fmt.Println(repo)
+
 	fmt.Println("- end")
 }
--- work/users-go/repositories/portfolio_repository.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-import (
-	"github.com/mezni/users-go/aggregates"
-)
-
-type PortfolioRepository interface {
-	Create(portfolio *aggregates.Portfolio) error
-}
