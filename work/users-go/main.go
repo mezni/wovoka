6bce363a8a314a3411ef181e930e47b5dca40491
Mezni <mamezni@gmail.com>
2024-05-27 13:57:09 +0000

    work 2024-05-27

--- work/users-go/main.go
+++ work/users-go/main.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("- start")
+type InputData struct {
+	Name   string
+	Limit  float64
+	Parent string
+}
 
+func main() {
+
+	data := []InputData{
+		InputData{Name: "default", Limit: 0, Parent: ""},
+		InputData{Name: "IT", Limit: 0, Parent: "default"},
+	}
+	fmt.Println("- start")
+	for _, k := range data {
+		fmt.Println(k)
+	}
 	fmt.Println("- end")
 }
