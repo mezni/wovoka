0af8c7779b7b2c77e8bea81070493eb9b3d6da1a
Mezni <mamezni@gmail.com>
2024-06-01 14:20:46 +0000

    work 2024-06-01

--- work/expenses-go/cmd/main.go
+++ work/expenses-go/cmd/main.go
@@ -4,6 +4,45 @@ import (
 	"fmt"
 )
 
+//momentum tech,2023-11-01,aws,367475994817,367475994817,Tax,,,,,,,,,,[],,54.41,USD
+
+type ExpenseInput struct {
+	Org      string
+	Period   string
+	Provider string
+	Account  string
+	Service  string
+	Cost     float64
+	Currency string
+}
+
+func contains(s []string, searchterm string) bool {
+	for _, v := range s {
+		if v == searchterm {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	fmt.Println("- start")
+	exspenses := []*ExpenseInput{
+		&ExpenseInput{"momentum tech", "2024-05-20", "aws", "367475994817", "ec2", 2.51, "USD"},
+		&ExpenseInput{"momentum tech", "2024-05-20", "aws", "367475994817", "s3", 4.75, "USD"},
+		&ExpenseInput{"momentum tech", "2024-05-21", "aws", "367475994817", "ec2", 1.91, "USD"},
+		&ExpenseInput{"momentum tech", "2024-05-21", "aws", "367475994817", "s3", 4.76, "USD"},
+	}
+	orgs := []string{}
+	periods := []string{}
+	for _, v := range exspenses {
+		if contains(orgs, v.Org) == false {
+			orgs = append(orgs, v.Org)
+		}
+		if contains(periods, v.Period) == false {
+			periods = append(periods, v.Period)
+		}
+	}
+	fmt.Println(orgs)
+	fmt.Println(periods)
 }
