523449cf253b18755ea6750632261a3c54c2de1f
Mezni <mamezni@gmail.com>
2024-06-02 23:03:24 +0000

    work 2024-06-02

--- work/expenses-go/cmd/main.go
+++ work/expenses-go/cmd/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	fmt.Println("- start")
-	records, err := csv.ReadCSV("./data/data.csv")
+	records, err := csv.ReadCSV("./data/_data.csv")
 	if err != nil {
 		log.Fatalf("Failed to read CSV file: %v", err)
 	}
@@ -16,5 +16,9 @@ func main() {
 	expenses, err := csv.ParseRecords(records)
 	if err != nil {
 		log.Fatalf("Failed to parse records: %v", err)
+	}
+
+	for _, v := range expenses {
+		fmt.Println(v)
 	}
 }
