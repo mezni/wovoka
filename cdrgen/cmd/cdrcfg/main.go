5f792caddbd9aa1f8e90debd9c8c341f73aa2c3c
Mezni <mamezni@gmail.com>
2024-12-29 00:55:54 +0000

    2024-12-29/0.0.37 Revamp

--- cdrgen/cmd/cdrcfg/main.go
+++ cdrgen/cmd/cdrcfg/main.go
@@ -1,49 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-
-	"gopkg.in/yaml.v3"
+    "fmt"
+    "os"
+    "gopkg.in/yaml.v3"
 )
 
 // LocationData represents the locations data
 type LocationData struct {
-	Locations struct {
-		Latitude  []float64 `yaml:"Latitude"`
-		Longitude []float64 `yaml:"Longitude"`
-		LocationSplit []struct {
-			NetworkTechnology string   `yaml:"NetworkTechnology"`
-			SplitRows          int      `yaml:"SplitRows"`
-			SplitColumns       int      `yaml:"SplitColumns"`
-			LocationNames      []string `yaml:"LocationNames"`
-		} `yaml:"LocationSplit"`
-	} `yaml:"Locations"`
+    Locations struct {
+        Latitude      []float64 `yaml:"Latitude"`
+        Longitude     []float64 `yaml:"Longitude"`
+        LocationSplit []struct {
+            NetworkTechnology string   `yaml:"NetworkTechnology"`
+            SplitRows          int      `yaml:"SplitRows"`
+            SplitColumns       int      `yaml:"SplitColumns"`
+            LocationNames      []string `yaml:"LocationNames"`
+        } `yaml:"LocationSplit"`
+    } `yaml:"Locations"`
 }
 
 func main() {
-	// Specify the file path
-	filePath := "config.yaml"
+    // Specify the file path
+    filePath := "config.yaml"
 
-	// Read the YAML file
-	yamlData, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		fmt.Printf("Error reading file %s: %v\n", filePath, err)
-		return
-	}
+    // Read the YAML file
+    yamlData, err := os.ReadFile(filePath)
+    if err != nil {
+        fmt.Printf("Error reading file '%s': %v\n", filePath, err)
+        return
+    }
 
-	// Unmarshal the YAML data into a LocationData struct
-	var locationData LocationData
-	err = yaml.Unmarshal(yamlData, &locationData)
-	if err != nil {
-		fmt.Printf("Error unmarshaling YAML: %v\n", err)
-		return
-	}
+    // Unmarshal the YAML data into a LocationData struct
+    var locationData LocationData
+    err = yaml.Unmarshal(yamlData, &locationData)
+    if err != nil {
+        fmt.Printf("Error unmarshaling YAML: %v\n", err)
+        return
+    }
 
-	// Access and print the data
-	fmt.Println("Latitude:", locationData.Locations.Latitude)
-	fmt.Println("Longitude:", locationData.Locations.Longitude)
-	for _,locationSplit := range locationData.Locations.LocationSplit {
-		fmt.Println("NetworkTechnology:", locationSplit.NetworkTechnology, locationSplit.SplitRows, locationSplit.SplitColumns, locationSplit.LocationNames)
-	}
+    // Access and print the data
+    fmt.Println("Latitude:", locationData.Locations.Latitude)
+    fmt.Println("Longitude:", locationData.Locations.Longitude)
+    for _, locationSplit := range locationData.Locations.LocationSplit {
+        fmt.Println("NetworkTechnology:", locationSplit.NetworkTechnology, locationSplit.SplitRows, locationSplit.SplitColumns, locationSplit.LocationNames)
+    }
 }
