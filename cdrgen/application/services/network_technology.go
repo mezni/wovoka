808b69ba094c222e28f3b36d554dfa5330f92acf
Mezni <mamezni@gmail.com>
2025-01-02 23:14:39 +0000

    2025-01-02/0.0.78 Revamp

--- cdrgen/application/services/network_technology.go
+++ cdrgen/application/services/network_technology.go
@@ -1,29 +1,26 @@
-package services
-
-import (
-	"github.com/mezni/wovoka/cdrgen/domain/entities"
-	"github.com/mezni/wovoka/cdrgen/infrastructure/sqlitestore"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-)
-
 // ConfigLoaderService handles the loading and saving of data.
 type ConfigLoaderService struct {
 	NetworkTechnologyRepo sqlitestore.NetworkTechnologyRepository
+	NetworkElementRepo    sqlitestore.NetworkElementRepository
+	ServiceTypeRepo       sqlitestore.ServiceTypeRepository
 }
 
 // NewConfigLoaderService creates a new instance of ConfigLoaderService.
-func NewConfigLoaderService(networkTechnologyRepo sqlitestore.NetworkTechnologyRepository) *ConfigLoaderService {
+func NewConfigLoaderService(
+	networkTechnologyRepo sqlitestore.NetworkTechnologyRepository,
+	networkElementRepo sqlitestore.NetworkElementRepository,
+	serviceTypeRepo sqlitestore.ServiceTypeRepository) *ConfigLoaderService {
+
 	return &ConfigLoaderService{
 		NetworkTechnologyRepo: networkTechnologyRepo,
+		NetworkElementRepo:    networkElementRepo,
+		ServiceTypeRepo:       serviceTypeRepo,
 	}
 }
 
 // LoadAndSaveData loads data from a JSON file and saves it to the database.
 func (app *ConfigLoaderService) LoadAndSaveData(filename string) error {
 	// Read the file content
-
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return fmt.Errorf("could not read file: %v", err)
@@ -32,7 +29,7 @@ func (app *ConfigLoaderService) LoadAndSaveData(filename string) error {
 	// Unmarshal JSON data
 	var jsonData struct {
 		NetworkTechnologies []entities.NetworkTechnology `json:"network_technologies"`
-		NetworkElements     []entities.NetworkElementType    `json:"network_element_types"`
+		NetworkElements     []entities.NetworkElement    `json:"network_element_types"`
 		ServiceTypes        []entities.ServiceType        `json:"service_types"`
 	}
 	if err := json.Unmarshal(data, &jsonData); err != nil {
@@ -41,9 +38,22 @@ func (app *ConfigLoaderService) LoadAndSaveData(filename string) error {
 
 	// Save network technologies to the database
 	for _, nt := range jsonData.NetworkTechnologies {
-		fmt.Println(nt)
 		if err := app.NetworkTechnologyRepo.Save(nt); err != nil {
 			return fmt.Errorf("error saving network technology: %v", err)
+		}
+	}
+
+	// Save network elements to the database
+	for _, ne := range jsonData.NetworkElements {
+		if err := app.NetworkElementRepo.Save(ne); err != nil {
+			return fmt.Errorf("error saving network element: %v", err)
+		}
+	}
+
+	// Save service types to the database
+	for _, st := range jsonData.ServiceTypes {
+		if err := app.ServiceTypeRepo.Save(st); err != nil {
+			return fmt.Errorf("error saving service type: %v", err)
 		}
 	}
 
--- cdrgen/domain/repositories/network_element_type.go
+++ cdrgen/domain/repositories/network_element_type.go
@@ -1,1 +1,16 @@
+
+
 package repositories
+
+import (
+	"github.com/mezni/wovoka/cdrgen/domain/entities"
+)
+
+
+type NetworkElementTypeRepository interface {
+	Save(elementType entities.NetworkElementType) error
+	GetAll() ([]entities.NetworkElementType, error)
+}
+
+
+
