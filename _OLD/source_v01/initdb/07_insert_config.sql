a7869b7a4153f6df44c0b7910da643cdbbd8acfb
mezni <mamezni@gmail.com>
2025-10-26 19:30:49 -0400

    Delete _OLD directory

--- _OLD/source_v01/initdb/07_insert_config.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO access_types (name, description) VALUES
-('public', 'Publicly accessible'),
-('private', 'Private access only'),
-('customers', 'Access restricted to customers');
-
-INSERT INTO data_sources (name, description) VALUES
-('osm', 'Data sourced from OpenStreetMap'),
-('user_submitted', 'Data submitted by users'),
-('commercial', 'Data from commercial providers');
